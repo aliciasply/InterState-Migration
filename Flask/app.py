f20eafe161db444473678fce429e712ba85dae04
Justin Acance <jacance@Justins-MacBook-Pro.local>
2021-02-19 21:45:41 -0800

    flask connected to db and route created

---
+++ Flask/app.py
@@ -0,0 +1,50 @@
+# Import dependencies
+# import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+# import datetime as dt
+
+# Create engine
+engine = create_engine("postgresql://postgres:postgres@localhost:5432/interstate_migration_db")
+
+# Reflect an existing database into a new model
+Base = automap_base()
+
+# Reflect the tables
+Base.prepare(engine, reflect=True)
+
+####### CHECK THIS #########
+States = Base.classes.states
+
+app = Flask(__name__)
+
+# Home page
+@app.route("/")
+def home():
+    print("Server received request for 'Home' page...")
+    return (
+        f"<h1>Welcome to PRISM, an analysis of Inter-state Migration!</h1><br/><br/>"
+        f'<b>Available Routes:</b><br/><br/>'
+        f'/api/v1.0/query<br/>'
+    )
+
+# Precipitation page
+@app.route("/api/v1.0/query")
+def precipitation():
+    session = Session(engine)
+    states_test = session.query(States.index).all()
+    results = []
+    state_source = {}
+    for x in states_test:
+        results.append({
+            "state_source" : x[0],
+        })
+    return jsonify(results)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ Flask/config.py
@@ -0,0 +1,5 @@
+#  Postgres password
+user = "postgres"
+pw = 'postgres'
+port = "5432"
+SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost:5432/interstate_migration_db"
