8ace7ccb4aa0d69b24e3a32f36e56095a4a45e75
Alexei Flores <alexei.r.flores@gmail.com>
2021-02-26 22:03:45 -0800

    latest sankey code added

--- static/js/sankey3.js
+++ static/js/sankey3.js
@@ -31,6 +31,7 @@ const sankey = {
   .nodeWidth(15)
   .nodePadding(10)
   .extent([[1, 5], [width - 1, height - 5]]),
+
   return ({nodes, links}) => sankey({
     nodes: nodes.map(d => Object.assign({}, d)),
     links: links.map(d => Object.assign({}, d))
@@ -45,6 +46,58 @@ var svg = d3.select("key")
 
 var chart = svg.append("g")
 .attr("viewBox", [0, 0, width, height]);
+attr("stroke", "#000")
+    .selectAll("rect")
+    .data(nodes)
+    .join("rect")
+      .attr("x", d => d.x0)
+      .attr("y", d => d.y0)
+      .attr("height", d => d.y1 - d.y0)
+      .attr("width", d => d.x1 - d.x0)
+      .attr("fill", color)
+    .append("title")
+      .text(d => `${d.name}\n${format(d.value)}`);
+
+      onst link = svg.append("g")
+      .attr("fill", "none")
+      .attr("stroke-opacity", 0.5)
+    .selectAll("g")
+    .data(links)
+    .join("g")
+      .style("mix-blend-mode", "multiply");
+
+      const gradient = link.append("linearGradient")
+        .attr("id", d => (d.uid = DOM.uid("link")).id)
+        .attr("gradientUnits", "userSpaceOnUse")
+        .attr("x1", d => d.source.x1)
+        .attr("x2", d => d.target.x0);
+
+        link.append("path")
+      .attr("d", d3.sankeyLinkHorizontal())
+      .attr("stroke", d => edgeColor === "none" ? "#aaa"
+          : edgeColor === "path" ? d.uid 
+          : edgeColor === "input" ? color(d.source) 
+          : color(d.destination))
+      .attr("stroke-width", d => Math.max(1, d.width));
+
+      ink.append("title")
+      .text(d => `${d.source.name} → ${d.destination.name}\n${format(d.value)}`);
+      svg.append("g")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", 10)
+    .selectAll("text")
+    .data(nodes)
+    .join("text")
+      .attr("x", d => d.x0 < width / 2 ? d.x1 + 6 : d.x0 - 6)
+      .attr("y", d => (d.y1 + d.y0) / 2)
+      .attr("dy", "0.35em")
+      .attr("text-anchor", d => d.x0 < width / 2 ? "start" : "end")
+      .text(d => d.name);
+
+  return svg.node();
+}
+
+
 
 d3.csv("static/data/sankey.csv").then(function(migrantData) {
     console.log (migrantData)
